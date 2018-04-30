15879fd3ca9824486bb942770371514330acedce
Christiebelle <cslester@outlook.com>
2018-04-30 20:54:32 +0100

    .use function added to the server file

--- routes/server.js
+++ routes/server.js
@@ -13,6 +13,13 @@ MongoClient.connect("mongodb://localhost:27017", function(err, client){
 
   const db = client.db("black_death_db");
 
+  router.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
   router.get("/facts", function(req, res){
     const factsCollection = db.collection("facts");
     factsCollection.find().toArray(function(err, facts){
